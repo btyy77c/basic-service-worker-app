a09defeebbaee8455d0ee17417ce8c084d8739bd
Emily Ring <btyy77c@gmail.com>
2018-06-13 17:19:44 -0400

    shorter sw

--- sw.js
+++ sw.js
@@ -38,22 +38,16 @@ self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
       .then(response => {
-        if (response) {
-          return response;
-        }
+        if (response) { return response }
 
         let fetchRequest = event.request.clone();
 
         return fetch(fetchRequest).then(response => {
-          if(!response || response.status !== 200 || response.type !== 'basic') {
-            return response;
-          }
+          if (!response || response.status !== 200 || response.type !== 'basic') { return response }
 
           let responseToCache = response.clone();
 
-          caches.open(CACHE_NAME).then(cache => {
-            cache.put(event.request, responseToCache);
-          });
+          caches.open(CACHE_NAME).then(cache => { cache.put(event.request, responseToCache) })
 
           return response;
         })
